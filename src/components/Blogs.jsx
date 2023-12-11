bad20a5247661a98f0cd1f7de6fe16e144015508
Shashank Naik <shashanknaik808@gmail.com>
2023-12-11 14:29:30 +0530

    Created a basic Blogs component using React class syntax

---
+++ src/components/Blogs.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Blogs extends Component {
+    render() {
+        return (
+            <div>Blogs</div>
+        )
+    }
+}
+
+export default Blogs;
