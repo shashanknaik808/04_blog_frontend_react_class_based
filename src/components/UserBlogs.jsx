49e3f8cd61a6170f5906c6c2459bbd869c6da371
Shashank Naik <shashanknaik808@gmail.com>
2023-12-11 14:32:19 +0530

    Created a basic UserBlogs component using React class syntax

---
+++ src/components/UserBlogs.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class UserBlogs extends Component {
+    render() {
+        return (
+            <div>UserBlogs</div>
+        )
+    }
+}
+
+export default UserBlogs;
