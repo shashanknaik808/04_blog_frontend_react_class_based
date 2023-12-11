ce7d178f4f1e3a44d963a36a551c2e0bf5a3313b
Shashank Naik <shashanknaik808@gmail.com>
2023-12-11 14:28:31 +0530

    Created a basic BlogDetail component using React class syntax

---
+++ src/components/BlogDetail.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class BlogDetail extends Component {
+    render() {
+        return (
+            <div>BlogDetail</div>
+        )
+    }
+}
+
+export default BlogDetail;
