265b2198802ed13cad73be1983b633674f25d61f
Shashank Naik <shashanknaik808@gmail.com>
2023-12-11 14:39:39 +0530

    Added routing functionality to the App component using react-router-dom and integrated login state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    }
+  }
   render() {
     return (
-      <header>
-        <Header />
-      </header>
+      <>
+        <header>
+          <Header isLoggedIn={this.state.isLoggedIn} />
+        </header>
+        <main>
+          <Routes>
+            <Route path='/auth' element={<Login />} />
+            <Route path='/blogs' element={<Blogs />} />
+            <Route path='/myBlogs' element={<UserBlogs />} />
+            <Route path='/myBlogs/:id' element={<BlogDetail />} />
+            <Route path='/blogs/add' element={<AddBlogs />} />
+          </Routes>
+        </main>
+      </>
     );
   }
 }
