f5c40cc7484e7a3eb6685fc52e74577a0c815c01
Shashank Naik <shashanknaik808@gmail.com>
2023-12-11 14:27:39 +0530

    Created a basic AddBlogs component using React class syntax

---
+++ src/components/AddBlogs.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class AddBlogs extends Component {
+    render() {
+        return (
+            <div>AddBlogs</div>
+        )
+    }
+}
+
+export default AddBlogs;
