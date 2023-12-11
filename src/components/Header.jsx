69e9c6b95bf249359a4f1530bd50b0238c148f6b
Shashank Naik <shashanknaik808@gmail.com>
2023-12-11 14:33:33 +0530

    Created a Header component with a basic navigation bar and login/sign up buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,27 @@ import React, { Component } from 'react';
 export class Header extends Component {
     render() {
         return (
-            <div>Header</div>
+            <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
+                <div className="container px-4 px-lg-5">
+                    <a className="navbar-brand" href="/">Blog App</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                        Menu
+                        <i className="fas fa-bars"></i>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav ms-auto py-4 py-lg-0">
+                            <li className="nav-item"><a href="/blogs" className="nav-link px-lg-3 py-3 py-lg-4">All Blogs</a></li>
+
+                            <li className="nav-item"><a href="/myBlogs" className="nav-link px-lg-3 py-3 py-lg-4">My Blogs</a></li>
+
+                            <li className="nav-item"><a href="/blogs/add" className="nav-link px-lg-3 py-3 py-lg-4">Add Blogs</a></li>
+
+                        </ul>
+                    </div>
+                    <button class="btn btn-warning me-2" type="button" >Login</button>
+                    <button class="btn btn-warning me-2" type="button" >Sign Up</button>
+                </div>
+            </nav>
         )
     }
 }
