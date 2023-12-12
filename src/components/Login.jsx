e90097a2c31f0ada15d32bb3a5e16e5f03d2045b
Shashank Naik <shashanknaik808@gmail.com>
2023-12-12 16:05:39 +0530

    Updated Login component to use react-router-dom's unstable_HistoryRouter for navigation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { unstable_HistoryRouter } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 
 export class Login extends Component {
