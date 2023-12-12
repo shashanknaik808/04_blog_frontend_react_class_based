a806a4622ec415d7710e953aaa77b61ee5af1fa3
Shashank Naik <shashanknaik808@gmail.com>
2023-12-12 15:35:48 +0530

    Updated AddBlogs component: Added header, form structure, and styling

--- src/components/AddBlogs.jsx
+++ src/components/AddBlogs.jsx
@@ -1,9 +1,55 @@
 import React, { Component } from 'react';
+// import { Link } from 'react-router-dom'
 
 export class AddBlogs extends Component {
     render() {
         return (
-            <div>Add Blogs</div>
+            <>
+                <header className="masthead" style={{ "backgroundImage": "url('assets/img/about-bg.jpg')" }}>
+                    <div className="container position-relative px-4 px-lg-5">
+                        <div className="row gx-4 gx-lg-5 justify-content-center">
+                            <div className="col-md-10 col-lg-8 col-xl-7">
+                                <div className="page-heading">
+                                    <h1>Add Blog</h1>
+                                    <span className="subheading">Add Your New Blogs</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+
+                <div className="container px-4 px-lg-5">
+                    <div className="row gx-4 gx-lg-5 justify-content-center">
+                        <div className="col-md-10 col-lg-8 col-xl-7">
+
+                            <form action="/blogs" method="POST" onSubmit={this.handleSubmit} >
+                                <div className="form-floating">
+                                    <input className="form-control" id="title" name="title" type="text"
+                                        placeholder="Enter the title..." onChange={this.handleChange} />
+                                    <label htmlFor="title">Title</label>
+                                </div>
+
+                                <div className="form-floating">
+                                    <textarea className="form-control" id="description" name="description"
+                                        placeholder="Enter your description here..." onChange={this.handleChange}></textarea>
+                                    <label htmlFor="description">Description</label>
+                                </div>
+
+                                <div className="form-floating">
+                                    <textarea className="form-control" id="image" name="image"
+                                        placeholder="Add Image" onChange={this.handleChange}></textarea>
+                                    <label htmlFor="image">Image</label>
+                                </div><br />
+
+                                {/* Submit Button*/}
+                                <div style={{ textAlign: 'center' }}>
+                                    <button id="submitButton" className="btn btn-primary text-uppercase" type="submit">Send</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </>
         )
     }
 }
