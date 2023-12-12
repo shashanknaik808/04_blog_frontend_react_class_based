a888ad2f75f45a6ea40f57fa3f8b8ed4fd39e934
Shashank Naik <shashanknaik808@gmail.com>
2023-12-12 15:31:09 +0530

    Updated Blogs component: Added header with background image and 'Older Posts' button

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+// import Blog from './Blog'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export class Blogs extends Component {
     render() {
         return (
-            <div>Blogs</div>
+            <>
+                <header className="masthead" style={{ "backgroundImage": "url('assets/img/about-bg.jpg')" }}>
+                    <div className="container position-relative px-4 px-lg-5">
+                        <div className="row gx-4 gx-lg-5 justify-content-center">
+                            <div className="col-md-10 col-lg-8 col-xl-7">
+                                <div className="page-heading">
+                                    <h1>Blogs</h1>
+                                    <span className="subheading">Display Blogs</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+
+                <div className="container px-4 px-lg-5">
+                    <div className="row gx-4 gx-lg-5 justify-content-center">
+                        <div className="col-md-10 col-lg-8 col-xl-7"
+
+                        >
+
+                            <div className="d-flex justify-content-end mb-4"><Link className="btn btn-primary text-uppercase"
+                                to="/">Older Posts →</Link></div>
+                        </div>
+                    </div>
+                </div>
+            </>
         )
     }
 }
