0365931e153a360b483bf174ac10adb6175759d7
Shashank Naik <shashanknaik808@gmail.com>
2023-12-12 13:24:00 +0530

    Created Signup component with form for user registration

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Header from './Header';
 
 export class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputs: {
+                name: "",
+                email: "",
+                password: ""
+            }
+
+        }
+    }
+
     render() {
         return (
-            <div>Signup</div>
+            <>
+                <Header banner="/assets/img/about-bg.jpg" heading="Signup Page" subHeading="Register a new User" />
+
+                <main className="mb-4">
+                    <div className="container px-4 px-lg-5">
+                        <div className="row gx-4 gx-lg-5 justify-content-center">
+                            <div className="col-md-10 col-lg-8 col-xl-7">
+
+                                <div className="my-5">
+                                    <form action="/auth/signup" method="POST" onSubmit={this.handleSubmit}>
+                                        <div className="form-floating">
+                                            <input className="form-control" id="name" name="name" type="text" onChange={this.handleChange} value={this.state.name}
+                                                placeholder="Type your Name..." />
+                                            <label htmlFor="name">Name</label>
+                                        </div><br />
+                                        <div className="form-floating">
+                                            <input className="form-control" id="email" name="email" type="email" onChange={this.handleChange} value={this.state.email}
+                                                placeholder="Type your registed email..." required />
+                                            <label htmlFor="email">Email</label>
+                                        </div><br />
+                                        <div className="form-floating">
+                                            <input className="form-control" id="password" name="password" type="password" onChange={this.handleChange} value={this.state.password}
+                                                placeholder="Type the password..." required />
+                                            <label htmlFor="password">Password</label>
+                                        </div><br />
+                                        {/* Submit Button*/}
+                                        <div style={{ textAlign: 'center' }}>
+                                            <button className="btn text-uppercase" id="submitButton" type="submit" style={{ color: 'orange' }}>Sign Up</button><br />
+                                        </div>
+
+                                        <span style={{ 'color': '#dc3545', 'fontWeight': 'bold', 'fontStyle': 'oblique' }}>
+                                            &ensp; &ensp;
+                                        </span>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </>
         )
     }
 }
