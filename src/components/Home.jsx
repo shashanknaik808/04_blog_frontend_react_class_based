9564995328f490be17f6785d39938355eeb22bb3
Shashank Naik <shashanknaik808@gmail.com>
2023-12-12 12:52:17 +0530

    Updated Home component with dynamic Header and added 'Older Posts' link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Header from './Header';
 
 export class Home extends Component {
     render() {
         return (
-            <div>Home</div>
+            <>
+                <Header banner="/assets/img/home-bg.jpg" heading="Filmy BLog" subHeading="Welcome to Chitralooka" />
+                <div className="container px-4 px-lg-5">
+                    <div className="row gx-4 gx-lg-5 justify-content-center">
+                        <div className="col-md-10 col-lg-8 col-xl-7">
+
+                            <div className="d-flex justify-content-end mb-4"><Link className="btn btn-primary text-uppercase"
+                                to="/">Older Posts →</Link></div>
+                        </div>
+                    </div>
+                </div>
+            </>
         )
     }
 }
