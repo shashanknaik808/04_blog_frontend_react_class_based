e46134ae21b1e09b9a8c5bdc7911b054284a2cc2
Shashank Naik <shashanknaik808@gmail.com>
2023-12-12 16:44:34 +0530

    Updated UserBlogs component to fix issues with API request and state

--- src/components/UserBlogs.jsx
+++ src/components/UserBlogs.jsx
@@ -8,13 +8,15 @@ export class UserBlogs extends Component {
         this.state = {
             user: '',
         }
-        const id = localStorage.getItem("userID");
 
         this.sendRequest = this.sendRequest.bind(this);
     }
 
-    async sendRequest() {
-        const res = await axios.post(`http://localhost:5000/api/blog/user/${this.id}`)
+    async sendRequest(id) {
+        // const id = localStorage.getItem("userID");
+        // console.log(id);
+        const res = await axios.get(`http://localhost:5000/api/blog/user/${id}`)
+            // .then(data => console.log(data))
             .catch(err => console.log(err))
 
         let data = null;
@@ -25,19 +27,34 @@ export class UserBlogs extends Component {
     }
 
     async componentDidMount() {
-        this.sendRequest().then(data => this.setState(data));
+        const id = localStorage.getItem("userID");
+        // console.log(id);
+        this.sendRequest(id).then(data => this.setState(data));
+        // console.log(this.state.user);
     }
 
     render() {
         return (
             <>
-                {" "}
+                <header className="masthead" style={{ "backgroundImage": "url('assets/img/about-bg.jpg')" }}>
+                    <div className="container position-relative px-4 px-lg-5">
+                        <div className="row gx-4 gx-lg-5 justify-content-center">
+                            <div className="col-md-10 col-lg-8 col-xl-7">
+                                <div className="page-heading">
+                                    <h1>Blog of {this.state.user.name}</h1>
+                                    <span className="subheading">View my Blogs</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+
                 {this.state.user && this.state.user.blogs && this.state.user.blogs.map((blog, index) => (
                     <Blog
                         key={index}
                         isUser={true}
                         id={blog._id}
-                        user={blog.user.name}
+                        user={this.state.user.name}
                         description={blog.description}
                         title={blog.title}
                         image={blog.image}
@@ -47,5 +64,4 @@ export class UserBlogs extends Component {
         )
     }
 }
-
 export default UserBlogs;
