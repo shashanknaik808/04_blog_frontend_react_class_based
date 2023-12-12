c7dc807fef8d608a613177b794ca487543b94bc6
Shashank Naik <shashanknaik808@gmail.com>
2023-12-12 15:43:54 +0530

    Refactored BlogDetail component: Updated the structure for displaying individual blog details with editing and deleting options

--- src/components/BlogDetail.jsx
+++ src/components/BlogDetail.jsx
@@ -3,7 +3,30 @@ import React, { Component } from 'react';
 export class BlogDetail extends Component {
     render() {
         return (
-            <div>Blog Detail</div>
+            <>
+                <header className="masthead" style={{ "backgroundImage": "url('assets/img/post-sample-image.jpg')" }}>
+                    <div className="container position-relative px-4 px-lg-5">
+                        <div className="row gx-4 gx-lg-5 justify-content-center">
+                            <div className="col-md-10 col-lg-8 col-xl-7">
+                                <div className="page-heading">
+                                    <h1>Blog Details</h1>
+                                    <span className="subheading">Edit or Delete Blog</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+
+                <div className="container px-4 px-lg-5">
+                    <div className="row gx-4 gx-lg-5 justify-content-center">
+                        <div className="col-md-10 col-lg-8 col-xl-7"
+
+                        >
+
+                        </div>
+                    </div>
+                </div>
+            </>
         )
     }
 }
