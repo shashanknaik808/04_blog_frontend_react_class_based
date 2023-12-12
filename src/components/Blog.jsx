5db9857d3690fd338ed21ac458ef5d9881675038
Shashank Naik <shashanknaik808@gmail.com>
2023-12-12 16:52:07 +0530

    Refactor Blog component with new header and article structure

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -10,20 +10,44 @@ export class Blog extends Component {
     render() {
         return (
             <>
-                <div className="post-preview" >
-                    <Link to={`/blog/${this.props.id}`}>
-                        <h2 className="post-title">{this.props.title}</h2>
-                        <h3 className="post-subtitle">{this.props.description}</h3>
-                        <h3 className="post-image">{this.props.imageURL}</h3>
-                    </Link>
+                <header class="masthead" style="background-image: url('assets/img/post-bg.jpg')">
+                    <div class="container position-relative px-4 px-lg-5">
+                        <div class="row gx-4 gx-lg-5 justify-content-center">
+                            <div class="col-md-10 col-lg-8 col-xl-7">
+                                <div class="post-heading">
+                                    <h1>Man must explore, and this is exploration at its greatest</h1>
+                                    <h2 class="subheading">Problems look mighty small from 150 miles up</h2>
+                                    <span class="meta">
+                                        Posted by
+                                        <a href="#!">Start Bootstrap</a>
+                                        on August 24, 2023
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <article class="mb-4">
+                    <div class="container px-4 px-lg-5">
+                        <div class="row gx-4 gx-lg-5 justify-content-center">
+                            <div class="col-md-10 col-lg-8 col-xl-7">
+                                <p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>
+                                <p>Science cuts two ways, of course; its products can be used for both good and evil. But there's no turning back from science. The early warnings about technological dangers also come from science.</p>
+                                <p>What was most significant about the lunar voyage was not that man set foot on the Moon but that they set eye on the earth.</p>
+                                <p>A Chinese tale tells of some men sent to harm a young girl who, upon seeing her beauty, become her protectors rather than her violators. That's how I felt seeing the Earth for the first time. I could not help but love and cherish her.</p>
+                                <p>For those who have seen the Earth from space, and for the hundreds and perhaps thousands more who will, the experience most certainly changes your perspective. The things that we share in our world are far more valuable than those which divide us.</p>
 
-                    <p className="post-meta">
-                        Posted by <Link to={`/otherBlogs/${this.props.isUser}`}>{this.props.user}</Link>
-                        &nbsp;
-                    </p>
-                </div>
-                {/* Divider*/}
-                <hr className="my-4" />
+
+                                <p>
+                                    Placeholder text by
+                                    <a href="http://spaceipsum.com/">Space Ipsum</a>
+                                    &middot; Images by
+                                    <a href="https://www.flickr.com/photos/nasacommons/">NASA on The Commons</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </article>
             </>
         )
     }
