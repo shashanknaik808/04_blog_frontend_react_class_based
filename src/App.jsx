10d3cadecf08ba979bcd14a35faca7e80135ed09
Shashank Naik <shashanknaik808@gmail.com>
2023-12-12 12:45:33 +0530

    Created Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
+import Home from './components/Home.jsx';
 import Footer from './components/Footer';
 import Login from './components/Login';
 import { Route, Routes } from 'react-router-dom';
