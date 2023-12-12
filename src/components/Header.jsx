03eaabd9fe15ee3c600d2954dc01cd70a9ad81e3
Shashank Naik <shashanknaik808@gmail.com>
2023-12-12 12:27:10 +0530

    Updated header in contact section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 export class Header extends Component {
     render() {
         return (
-            <div>Header</div>
+            <>
+                <header class="masthead" style={{ 'background-image': "url('assets/img/contact-bg.jpg')" }}>
+                    <div class="container position-relative px-4 px-lg-5">
+                        <div class="row gx-4 gx-lg-5 justify-content-center">
+                            <div class="col-md-10 col-lg-8 col-xl-7">
+                                <div class="page-heading">
+                                    <h1>Contact Me</h1>
+                                    <span class="subheading">Have questions? I have answers.</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+            </>
         )
     }
 }
