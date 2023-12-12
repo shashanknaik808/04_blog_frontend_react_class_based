aa96b04992b02f606c7483ede7a0c816b36510ee
Shashank Naik <shashanknaik808@gmail.com>
2023-12-12 12:43:13 +0530

    Created Blog component

---
+++ src/components/Blog.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Blog extends Component {
+    render() {
+        return (
+            <div>Blog</div>
+        )
+    }
+}
+
+export default Blog;
