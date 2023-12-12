c76bf34f7b6dfc6f1e53368eefc7d3efa5c95be1
Shashank Naik <shashanknaik808@gmail.com>
2023-12-12 13:05:37 +0530

    Added JavaScript code for handling scroll events and updating navigation bar visibility

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ export class Home extends Component {
     render() {
         return (
             <>
-                <Header banner="/assets/img/home-bg.jpg" heading="Filmy BLog" subHeading="Welcome to Chitralooka" />
+                <Header banner="/assets/img/home-bg.jpg" heading="Filmy Blog" subHeading="Welcome to Chitraloka" />
                 <div className="container px-4 px-lg-5">
                     <div className="row gx-4 gx-lg-5 justify-content-center">
                         <div className="col-md-10 col-lg-8 col-xl-7">
