6eda1ffdecef68b9945c40d2db2057f019d14f97
Shashank Naik <shashanknaik808@gmail.com>
2023-12-12 15:22:52 +0530

    Updated App component: Added new routes for Blogs, AddBlogs, UserBlogs, and BlogDetail components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import Login from './components/Login';
 import { Route, Routes } from 'react-router-dom';
 import Logout from './components/Logout';
 import Signup from './components/Signup';
+import AddBlogs from './components/AddBlogs';
+import Blogs from './components/Blogs';
+import UserBlogs from './components/UserBlogs';
+import BlogDetail from './components/BlogDetail';
 
 export class App extends Component {
   constructor(props) {
@@ -21,9 +25,14 @@ export class App extends Component {
         <Navbar isLoggedIn={this.state.isLoggedIn} />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/auth' element={<Login />}></Route>
-          <Route path='/auth/logout' element={<Logout />}></Route>
-          <Route path='/auth/signup' element={<Signup />}></Route>
+          <Route path='/auth' element={<Login />} />
+          <Route path='/auth/logout' element={<Logout />} />
+          <Route path='/auth/signup' element={<Signup />} />
+
+          <Route path='/blogs' element={<Blogs />} />
+          <Route path='/blog/add' element={<AddBlogs />} />
+          <Route path='/myBlogs' element={<UserBlogs />} />
+          <Route path='/myBlogs/:id' element={<BlogDetail />} />
         </Routes>
         <Footer />
 
