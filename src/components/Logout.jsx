b804fe55fe72d977b7d7472ed4c4a2fb19f841e9
Shashank Naik <shashanknaik808@gmail.com>
2023-12-12 14:30:29 +0530

    Refactor Logout component: added logout functionality and link to auth page

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
 export class Logout extends Component {
+
+    constructor() {
+        super();
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    handleLogout() {
+        this.props.isLoggedIn(false)
+        localStorage.clear();
+
+    }
+
     render() {
         return (
-            <div>Logout</div>
+            <div onClick={this.handleLogout}>
+                <Link to='/auth'></Link>
+                Thank You
+            </div>
         )
     }
 }
