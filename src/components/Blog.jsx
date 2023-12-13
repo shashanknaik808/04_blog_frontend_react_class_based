eb352dc11d0294fe1541738dd2c07eba21dcf9c1
Shashank Naik <shashanknaik808@gmail.com>
2023-12-13 11:33:41 +0530

    Refactor Blog component to fetch blog details dynamically using axios

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,55 +1,81 @@
+import axios from 'axios';
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-export class Blog extends Component {
+class Blog extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            blog: null,
+        };
+    }
 
-    // constructor(props){
-    //   super(props);
-    // }
+    async fetchDetails(id) {
+        try {
+            const res = await axios.get(`http://localhost:5000/api/blog/${id}`);
+            const data = res.data;
+            return data;
+        } catch (err) {
+            console.log("Data not getting", err);
+            return null;
+        }
+
+    }
+
+    async componentDidMount() {
+        const idArr = document.URL.split('/');
+        let id = idArr[idArr.length - 1];
+
+        const data = await this.fetchDetails(id);
+        if (data) {
+            this.setState({ blog: data.blog });
+        }
+    }
 
     render() {
+
         return (
-            <>
-                <header class="masthead" style="background-image: url('assets/img/post-bg.jpg')">
-                    <div class="container position-relative px-4 px-lg-5">
-                        <div class="row gx-4 gx-lg-5 justify-content-center">
-                            <div class="col-md-10 col-lg-8 col-xl-7">
-                                <div class="post-heading">
-                                    <h1>Man must explore, and this is exploration at its greatest</h1>
-                                    <h2 class="subheading">Problems look mighty small from 150 miles up</h2>
-                                    <span class="meta">
-                                        Posted by
-                                        <a href="#!">Start Bootstrap</a>
-                                        on August 24, 2023
+            <> {(this.state.blog === null) ? <h1>Loading</h1> :
+                (<div><header className="masthead" style={{ 'backgroundImage': "url(" + this.state.blog.image + " )" }}>
+                    <div className="container position-relative px-4 px-lg-5">
+                        <div className="row gx-4 gx-lg-5 justify-content-center">
+                            <div className="col-md-10 col-lg-8 col-xl-7">
+                                <div className="post-heading">
+                                    <h1>{this.state.blog.title}</h1>
+                                    <span className="meta">
+                                        Posted by {this.state.blog.user.name}
+                                        on xxxxxxxx
                                     </span>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </header>
-                <article class="mb-4">
-                    <div class="container px-4 px-lg-5">
-                        <div class="row gx-4 gx-lg-5 justify-content-center">
-                            <div class="col-md-10 col-lg-8 col-xl-7">
-                                <p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>
-                                <p>Science cuts two ways, of course; its products can be used for both good and evil. But there's no turning back from science. The early warnings about technological dangers also come from science.</p>
-                                <p>What was most significant about the lunar voyage was not that man set foot on the Moon but that they set eye on the earth.</p>
-                                <p>A Chinese tale tells of some men sent to harm a young girl who, upon seeing her beauty, become her protectors rather than her violators. That's how I felt seeing the Earth for the first time. I could not help but love and cherish her.</p>
-                                <p>For those who have seen the Earth from space, and for the hundreds and perhaps thousands more who will, the experience most certainly changes your perspective. The things that we share in our world are far more valuable than those which divide us.</p>
-
-
-                                <p>
-                                    Placeholder text by
-                                    <a href="http://spaceipsum.com/">Space Ipsum</a>
-                                    &middot; Images by
-                                    <a href="https://www.flickr.com/photos/nasacommons/">NASA on The Commons</a>
-                                </p>
+                    <article className="mb-4">
+                        <div className="container px-4 px-lg-5">
+                            <div className="row gx-4 gx-lg-5 justify-content-center">
+                                <div className="col-md-10 col-lg-8 col-xl-7">
+                                    <p>{this.state.blog.description}</p>
+                                    {/* <img>{this.state.blog.image}</img> */}
+                                    {(this.state.blog.user._id === localStorage.getItem("userID")) && (
+                                        <>
+                                            <div style={{ padding: "20px" }}>
+                                                <Link to={`/editBlog/${this.state.blog._id}`}>
+                                                    <i className="fa-solid fa-pen-to-square"></i>
+                                                </Link>
+                                                <i className="fa-solid fa-trash"></i>
+                                            </div>
+                                        </>
+                                    )}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </article>
+                    </article>
+                </div>
+                )
+            }
             </>
-        )
+        );
     }
 }
 
